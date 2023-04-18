58afb59866c8dfe9f5a66d04d4be20d8644b768c
maksim-solovev <maksim-solovev@noreply.com>
2023-04-18 19:26:25 +0300

    renamed standart reaction to the barrier react

---
+++ src/grid/react/barrier.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "react.hpp"
+#include "../../assertions.h"
+
+namespace grid {
+    namespace react {
+        class barrier : public react{
+            public:
+            double barrier_height;
+            double freq;
+            public:
+                inline barrier(std::shared_ptr<atom::Type> f,std::shared_ptr<atom::Type> s,std::shared_ptr<atom::Type> _to1, std::shared_ptr<atom::Type> _to2,double _maxdist,double _barrier,double _freq):react(f,s,_to1,_to2,_maxdist),barrier_height(_barrier),freq(_freq) {};
+
+                virtual double calcDE(std::shared_ptr<grid::atom::Atom>& f, std::shared_ptr<grid::atom::Atom>& s) const;
+
+                inline virtual double getCutoff() const{
+                    return sgs::ELVOLT*20;
+                }
+
+                virtual double getDEChecked(std::shared_ptr<grid::atom::Atom>& f, std::shared_ptr<grid::atom::Atom>& s) const;
+
+                virtual double Chance(std::shared_ptr<grid::atom::Atom>& f, std::shared_ptr<grid::atom::Atom>& s, double distance) const;
+
+                virtual void Apply(std::shared_ptr<grid::atom::Atom>& f, std::shared_ptr<grid::atom::Atom>& s) const ;
+        };
+    }
+}
